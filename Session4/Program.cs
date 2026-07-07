0c2793657a0f699e329e77e3a19744b05b588bcd
ammaralazki <alazkiyasser3@gmail.com>
2026-07-07 15:59:52 +0400

    Initial commit

---
+++ Session4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Session4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ask the user for their name
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        // Call the function
+        PrintWelcome(userName);
+
+
+        // Function definition
+        static void PrintWelcome(string name)
+        {
+            Console.WriteLine($"Hello {name}, welcome! It is great to have you here.");
+        }
+        
+        
+    }
+}    
